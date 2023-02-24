9eb996c6e0c55685160b8998b98337dc019df97d
Theawesometurtl <shablammmo@gmail.com>
2023-02-24 11:45:37 -0800

    added a ton of comments

--- speed sensor idea/speed sensor idea.ino
+++ speed sensor idea/speed sensor idea.ino
@@ -1,11 +1,22 @@
 /*
-we still have two problems, the arduino doesn't have enough ram to store enough data, and the arduino doesn't have
-access to the actual time of day. These could both be solved by connecting to wifi, but we might not be close enough to wifi
+The code so far works like so:
 
-There are ways to add additional ram to the arduino, but I'm not sure if there's a solution to the time problem.
-We can also add a clock to the arduino, something like a ds3231, but it might be difficult
+a car runs our first pressure sensor starting a timer
 
-Basically, we either need a wifi adapter chip, or both a RAM chip and a RTC (realtime clock) chip
+no data will be stored about the until slowestCarWheel time has passed, 100 loops, in this case, meaning nothing
+will be triggered if the car is detected on the first pressure sensor multiple times
+
+the car runs over the second pressure sensor
+since the timer is still running, we can use our info about the car to calculate speed and determine direction
+this data is now stored in an array, and printed to the serial monitor
+
+(Alex) I am planning to write a python function that will be able to read our data through a serial port and format
+it nicely into a file. This python function will be in a different folder. In order for that to work, we would
+need to create a function on the arduino to print our array to the serial monitor. I don't know how we would trigger
+this function to print out the array, but I'm assuming we would use a button, which would be one more thing to add
+onto each arduino
+
+
 */
 const int minPressure = 10;
 const int slowestCar = 10000;
@@ -15,6 +26,7 @@ const int distance = 100;
 int times[100];
 int speeds[100];
 int carsPassed = 0;
+//we have two seperate timers so we can find the direction a car is going
 int timer1 = 0;
 int timer2 = 0;
 int pressure;
@@ -25,6 +37,7 @@ void static collectData(int: time, bool: direction) {
   Serial.println(time + " " + speed);
   times[carsPassed] = 1; //IDK how to access realtime, this might be a huge problem
   speeds[carsPassed] = speed;
+  carspassed++;
 }
 
 bool static timerStarted(timer) {
@@ -33,6 +46,8 @@ bool static timerStarted(timer) {
   }
   return false
 }
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -48,9 +63,12 @@ void loop() {
   }
   
   if (pressure1 > minPressure) {
+    //timer started function checks if the timer is in a certain range, meaning it would have been triggered already
     if (timerStarted(timer2)) {
-      collectData(timer2, direction);
+      //store the data about the cars passed, direction, and speed
+      collectData(timer2, true);//the bool is for the direction the car went in, I don't know a better way to specify direction
     } else {
+      //if the timer hasn't been reset yet, it gets reset now
       timer1 = 0;
     }
     
@@ -59,9 +77,12 @@ void loop() {
   }
 
   if (pressure2 > minPressure) {
+    //timer started function checks if the timer is in a certain range, meaning it would have been triggered already
     if (timerStarted(timer1)) {
-      collectData(timer2, direction);
+      //store the data about the cars passed, direction, and speed
+      collectData(timer2, false);//the bool is for the direction the car went in, I don't know a better way to specify direction
     } else {
+      //if the timer hasn't been reset yet, it gets reset now
       timer2 = 0;
     }
 
